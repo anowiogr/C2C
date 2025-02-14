376ff9deb26fbcae1e7110768e92d4e764da991d
bielak84 <69897588+bielak84@users.noreply.github.com>
2025-02-14 13:35:23 +0100

    Revert "Update base URL in config and adjust config path in header"
    This reverts commit 7c19566f79c2918d9779dff8c9e541d0976b8eb1.

--- config/config.php
+++ config/config.php
@@ -5,6 +5,6 @@ return [
     'user' => 'administrator',
     'password' => 'c2cPr0j3kt',
     'basedir' => 'C:/xamp/htdocs/inż/C2C',
-    'baseurl' => 'http://localhost/inż/C2C'
+    'baseurl' => 'localhost/inż/C2C'
 ];
 ?> 
