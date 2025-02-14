ab911de469bbe0aae80b0ec6cb357fa9768f2f03
bielak84 <69897588+bielak84@users.noreply.github.com>
2025-02-14 14:46:14 +0100

    Update base directory and base URL in config file

--- config/config.php
+++ config/config.php
@@ -4,7 +4,7 @@ return [
     'dbname' => 'c2c',
     'user' => 'administrator',
     'password' => 'c2cPr0j3kt',
-    'basedir' => 'C:/xamp/htdocs/inż/C2C',
-    'baseurl' => 'localhost/inż/C2C'
+    'basedir' => 'C:\Program Files\XAMPP\htdocs\inż\C2C',
+    'baseurl' => 'http://localhost/inż/C2C'
 ];
 ?> 
