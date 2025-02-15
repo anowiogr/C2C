c9ba3054c64c0aabd1050fd0a8fc6018d7adbd26
bielak84 <69897588+bielak84@users.noreply.github.com>
2025-02-15 14:57:36 +0100

    Refactor auction view layout and improve search auction logic

--- mvc/view/auctions/auction.php
+++ mvc/view/auctions/auction.php
@@ -33,29 +33,28 @@ try {
                     </div>
 
                     <div class="p-3 mb-2 col-md-6" style="text-align: right;">
+                        <h4>
+                            <b>Sprzedający:</b> <?php echo $auction["login"]; ?><br><br>
+                            <b>Telefon:</b> <?php echo ($_SESSION["role"]=="guest" ?  "Zaloguj się aby zobaczyć nr telefonu" : $auction['phone']); ?> <br><br>
+                        </h4>
+                        <a class='btn btn-secondary' <?php echo "href='../messages/newmessage.php?auction_id=$auctionId'"; ?> >
+                        <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-send' viewBox='0 0 16 16'>
+                            <path d='M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z'/>
+                        </svg> Napisz wadomość </a>
 
-                            <h4>
-                                <b>Sprzedający:</b> <?php echo $auction["login"]; ?><br><br>
-                                <b>Telefon:</b> <?php echo ($_SESSION["role"]=="guest" ?  "Zaloguj się aby zobaczyć nr telefonu" : $auction['phone']); ?> <br><br>
-                            </h4>
-                            <a class='btn btn-secondary' <?php echo "href='../messages/newmessage.php?auction_id=$auctionId'"; ?> >
-                            <svg xmlns='http://www.w3.org/2000/svg' width='16' height='16' fill='currentColor' class='bi bi-send' viewBox='0 0 16 16'>
-                                <path d='M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576 6.636 10.07Zm6.787-8.201L1.591 6.602l4.339 2.76 7.494-7.493Z'/>
-                            </svg> Napisz wadomość </a>
+                        <?php
+                        if($_SESSION["role"]<>"guest"){
+                         echo   <<< BUTTONBUY
+                         <a class='btn btn-danger' <?php echo "href='../../controller/buyauction.php?auction_id=$auctionId'"; ?>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
+                        <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
+                        </svg> Kupuję </a>
+                        BUTTONBUY;
+                        }
+                        ?>
 
-                            <?php
-                            if($_SESSION["role"]<>"guest"){
-                             echo   <<< BUTTONBUY
-                             <a class='btn btn-danger' <?php echo "href='../../controller/buyauction.php?auction_id=$auctionId'"; ?>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bag" viewBox="0 0 16 16">
-                            <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z"/>
-                            </svg> Kupuję </a>
-                            BUTTONBUY;
-                            }
-                            ?>
-
-                            <br><br><br>
-                            <h2><?php echo $auction["price"]." ".$auction["currency_name"]; ?></h2>
+                        <br><br><br>
+                        <h2><?php echo $auction["price"]." ".$auction["currency_name"]; ?></h2>
 
                     </div>
 
@@ -86,30 +85,37 @@ try {
         }
     } else {
 
-                //Pobranie przefiltrowanych aukcji z podstawowymi informacjami
-                if(isset($_SESSION["filter"]["search"])&& $_SESSION["filter"]["search"]<>null&& $_SESSION["filter"]["search"]<>''){
+        // Pobranie przefiltrowanych aukcji z podstawowymi informacjami
+        if(isset($_SESSION["filter"]["search"])&& $_SESSION["filter"]["search"]<>null&& $_SESSION["filter"]["search"]<>''){
 
-                    $searchbar="'%".$_SESSION["filter"]["search"]."%'";
-                    print_r($searchbar."<br>");
+            $searchbar="'%".$_SESSION["filter"]["search"]."%'";
+            print_r($searchbar."<br>");
 
-                    $stmt = $model -> getAuctionBySearch($searchbar);
+            $stmt = $model -> getAuctionBySearch($searchbar);
 
-                    $_SESSION["filter"]["search"]=null;
+            $_SESSION["filter"]["search"]=null;
 
-                    } elseif(isset($_SESSION["filter"]["categoryid"])&& $_SESSION["filter"]["categoryid"]<>null) {
+        } elseif(isset($_SESSION["filter"]["categoryid"])&& $_SESSION["filter"]["categoryid"]<>null) {
 
+            $categoryid = $_SESSION["filter"]["categoryid"];
+            //print_r($categoryid . "<br>");
 
-                    $categoryid = $_SESSION["filter"]["categoryid"];
-                    //print_r($categoryid . "<br>");
+            $stmt = $model -> getAuctionByCategory($categoryid);
 
-                    $stmt = $model -> getAuctionByCategory($categoryid);
+            $_SESSION["filter"]["categoryid"]=null;
 
-                    $_SESSION["filter"]["categoryid"]=null;
+            $auctions = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-                } else {
-                    $stmt = $model -> getAllAuction();
-                }
+            if (empty($auctions)) {
+                echo "<div class='alert alert-warning' role='alert'>Nie ma żadnych aukcji w tej kategorii. Oto inne dostępne aukcje:</div>";
+                $stmt = $model -> getAllAuction();
                 $auctions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+            }
+
+        } else {
+            $stmt = $model -> getAllAuction();
+            $auctions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
 
         foreach ($auctions as $auction) {
             echo <<< TABLELISTA
@@ -136,7 +142,6 @@ try {
 
         } echo "<br>";
     }
-
 
 } catch (PDOException $e) {
     die("Błąd połączenia lub tworzenia bazy danych: " . $e->getMessage());
